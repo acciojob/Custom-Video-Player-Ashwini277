729b6906eb32a201bfea608aa123c5d7aa1fef54
Ashwini277 <devarushiashwini@gmail.com>
2024-08-29 18:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,8 @@
-// Get elements
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
-
-// Functions
-function togglePlay() {
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
-}
-
-function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    toggle.textContent = icon;
-}
-
-function skip() {
-    video.currentTime += parseFloat(this.dataset.skip);
-}
-
-function handleRangeUpdate() {
-    video[this.name] = this.value;
-}
-
-function handleProgress() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
-}
-
-function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-    video.currentTime = scrubTime;
-}
-
-// Event listeners
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
-
-toggle.addEventListener('click', togglePlay);
-
-skipButtons.forEach(button => button.addEventListener('click', skip));
-
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
-
-let mousedown = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);
+it('check if the diff components exist', () => {
+    cy.get('video').first().should('have.attr', 'src').should('include', 'video.mp4');
+    cy.get('.progress__filled');
+    cy.get('.player__button.toggle');
+    cy.get('input').should('have.length', 2);
+    cy.contains('« 10s');
+    cy.contains('25s »');
+});
